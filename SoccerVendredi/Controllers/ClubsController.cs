9dd6b61513b36480d18a9e59730e9fbd0bd374a7
Sulliivan <qraks-@hotmail.fr>
2020-11-13 12:27:12 +0100

    controller

---
+++ SoccerVendredi/Controllers/ClubsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SoccerVendredi.Context;
+
+namespace SoccerVendredi.Controllers
+{
+    public class ClubsController : Controller
+    {
+        private SoccerEntities db = new SoccerEntities();
+
+        // GET: Clubs
+        public ActionResult Index()
+        {
+            return View(db.Club.ToList());
+        }
+
+        // GET: Clubs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Club club = db.Club.Find(id);
+            if (club == null)
+            {
+                return HttpNotFound();
+            }
+            return View(club);
+        }
+
+        // GET: Clubs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clubs/Create
+        // Afin de déjouer les attaques par survalidation, activez les propriétés spécifiques auxquelles vous voulez établir une liaison. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,NameClub,City")] Club club)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Club.Add(club);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(club);
+        }
+
+        // GET: Clubs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Club club = db.Club.Find(id);
+            if (club == null)
+            {
+                return HttpNotFound();
+            }
+            return View(club);
+        }
+
+        // POST: Clubs/Edit/5
+        // Afin de déjouer les attaques par survalidation, activez les propriétés spécifiques auxquelles vous voulez établir une liaison. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,NameClub,City")] Club club)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(club).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(club);
+        }
+
+        // GET: Clubs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Club club = db.Club.Find(id);
+            if (club == null)
+            {
+                return HttpNotFound();
+            }
+            return View(club);
+        }
+
+        // POST: Clubs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Club club = db.Club.Find(id);
+            db.Club.Remove(club);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SoccerVendredi/Controllers/PlayersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SoccerVendredi.Context;
+
+namespace SoccerVendredi.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private SoccerEntities db = new SoccerEntities();
+
+        // GET: Players
+        public ActionResult Index()
+        {
+            var player = db.Player.Include(p => p.Club);
+            return View(player.ToList());
+        }
+
+        // GET: Players/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Player.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // GET: Players/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClubId = new SelectList(db.Club, "Id", "NameClub");
+            return View();
+        }
+
+        // POST: Players/Create
+        // Afin de déjouer les attaques par survalidation, activez les propriétés spécifiques auxquelles vous voulez établir une liaison. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Firstname,Lastname,Number,ClubId")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Player.Add(player);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClubId = new SelectList(db.Club, "Id", "NameClub", player.ClubId);
+            return View(player);
+        }
+
+        // GET: Players/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Player.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClubId = new SelectList(db.Club, "Id", "NameClub", player.ClubId);
+            return View(player);
+        }
+
+        // POST: Players/Edit/5
+        // Afin de déjouer les attaques par survalidation, activez les propriétés spécifiques auxquelles vous voulez établir une liaison. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Firstname,Lastname,Number,ClubId")] Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(player).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClubId = new SelectList(db.Club, "Id", "NameClub", player.ClubId);
+            return View(player);
+        }
+
+        // GET: Players/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Player player = db.Player.Find(id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            return View(player);
+        }
+
+        // POST: Players/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Player player = db.Player.Find(id);
+            db.Player.Remove(player);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
